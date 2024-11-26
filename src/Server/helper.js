12990e96f9ad28b4618c0ab5cc9a591997e1b339
ibraheem <ibraheem45622@gmail.com>
2024-11-26 15:34:42 +0500

    first commit

--- src/Server/helper.js
+++ src/Server/helper.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://moviesmanagementapp-be.onrender.com"
+export const BACKEND_URL = "https://moviesmanagementapp-be.onrender.com/"
 
 // export const BACKEND_URL = "http://localhost:5000/"
